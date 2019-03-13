27114facb0fca74c3d5c6b197dd8b66bff7ebf74
jeannegreulich <jeanne.greulich@onyxpoint.com>
2019-03-13 10:40:37 -0400

    fix listener spec test

--- spec/defines/v3/listener_spec.rb
+++ spec/defines/v3/listener_spec.rb
@@ -15,18 +15,18 @@ describe 'freeradius::v3::listener', :type => :define do
 
         it { is_expected.to contain_concat('listener.test1') }
         it { is_expected.to contain_freeradius__v3__listen('/etc/raddb/conf.d/listener.test1-fragment')
-#             .with({
-#          :target             => '/etc/raddb/conf.d/listener.test1',
-#          :order              => 100,
-#          :listen_type        => 'auth',
-#          :ipaddr             => 'ALL',
-#          :port               => :undef,
-#          :interface          => :undef,
-#          :per_socket_clients => :undef,
-#          :max_pps            => :undef,
-#          :max_connections    => :undef,
-#          :idle_timeout       => :undef
-#        })
+             .with({
+          :target             => '/etc/raddb/conf.d/listener.test1',
+          :order              => 100,
+          :listen_type        => 'auth',
+          :ipaddr             => 'ALL',
+          :port               => nil,
+          :interface          => nil,
+          :per_socket_clients => nil,
+          :max_pps            => nil,
+          :max_connections    => nil,
+          :idle_timeout       => nil
+        })
         }
 
       end
