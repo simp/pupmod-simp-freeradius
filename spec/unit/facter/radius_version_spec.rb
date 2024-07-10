23b9bb30ec2b49ee7df4256971567a39f18d617b
Steven Pritchard <steve@sicura.us>
2024-07-10 12:33:30 -0500

    Switch from mocha to rspec mocks

--- spec/unit/facter/radius_version_spec.rb
+++ spec/unit/facter/radius_version_spec.rb
@@ -8,8 +8,8 @@ describe 'radius_version', :type => :fact do
 
   context 'when freeradius is installed' do
     it 'should return a value' do
-      Facter::Core::Execution.expects(:which).with('radiusd').returns('/usr/sbin/radiusd')
-      Facter::Core::Execution.expects(:exec).with('/usr/sbin/radiusd -v').returns(File.read File.expand_path('../../../files/radiusd_version_info', __FILE__))
+      allow(Facter::Core::Execution).to receive(:which).with('radiusd').and_return('/usr/sbin/radiusd')
+      allow(Facter::Core::Execution).to receive(:exec).with('/usr/sbin/radiusd -v').and_return(File.read File.expand_path('../../../files/radiusd_version_info', __FILE__))
 
       expect(Facter.fact(:radius_version).value).to eq '3.0.13'
     end
@@ -17,8 +17,8 @@ describe 'radius_version', :type => :fact do
 
   context 'when radiusd returns junk' do
     it 'should return unknown' do
-      Facter::Core::Execution.expects(:which).with('radiusd').returns('/usr/sbin/radiusd')
-      Facter::Core::Execution.expects(:exec).with('/usr/sbin/radiusd -v').returns('This is not the radius you are looking for')
+      allow(Facter::Core::Execution).to receive(:which).with('radiusd').and_return('/usr/sbin/radiusd')
+      allow(Facter::Core::Execution).to receive(:exec).with('/usr/sbin/radiusd -v').and_return('This is not the radius you are looking for')
 
       expect(Facter.fact(:radius_version).value).to eq 'unknown'
     end
